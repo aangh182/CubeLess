bd2293192de2b342457775e20d0b5e5c66f9240e
Nguyễn Hoàng Ân <24520056@gm.uit.edu.vn>
2025-12-19 19:59:43 +0700

    Add PWA support by introducing manifest, service worker, and icon files, and registering them in index.html.

---
+++ sw.js
@@ -0,0 +1,39 @@
+const CACHE_NAME = 'cubeless-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './cubeless.css',
+  './js/cubeless.js',
+  './icons/icon.png',
+  './manifest.json'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keyList) => {
+      return Promise.all(
+        keyList.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+});
